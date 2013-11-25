1d6751547ef8681c0a7a4a57dedd2e14c40eda49
Julia Evans <julia@jvns.ca>
2013-11-25 10:48:53 -0500

    Initial commit

---
+++ src/change_ringing/core.clj
@@ -0,0 +1,6 @@
+(ns change-ringing.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/change_ringing/core_test.clj
@@ -0,0 +1,7 @@
+(ns change-ringing.core-test
+  (:require [clojure.test :refer :all]
+            [change-ringing.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
