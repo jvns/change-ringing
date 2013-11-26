e9855c4b76c761aca0df7568c053ece376742b2b
Julia Evans <julia@jvns.ca>
2013-11-26 12:18:48 -0500

    Add a magical metronome

--- src/change_ringing/core.cljs
+++ src/change_ringing/core.cljs
@@ -1,4 +1,8 @@
-(ns change-ringing.core)
+(ns change-ringing.core
+  (:require [cljs.core.async :as async
+             :refer [timeout dropping-buffer chan]])
+  (:require-macros [cljs.core.async.macros :refer [go]]))
+
 (js/console.log (+ 3 2))
 
 (def api-key "b1de6c92bba14d96a9fd957696c8cc4e")
@@ -13,8 +17,30 @@
                   (let [snd (-> sound
                                 (aget "preview-hq-mp3")
                                 (js/Audio.))]
-                    (aset sound-cache name sound))))))
+                    (aset sound-cache name snd))))))
 
-;; (get-sound "C4" 143214)
+(get-sound "note" 148501)
+
+(def sound-chan (chan (dropping-buffer 1000)))
+
+(defn play-timer
+  ([] (play-timer 250))
+  ([interval]
+     (go (while true
+           (js/console.log sound-cache)
+           (when-let [name (<! sound-chan)]
+             (when-let [sound (aget sound-cache name)]
+               (.play sound)
+               (.log js/console interval (<! (timeout interval)))
+               (aset sound "currentTime" 0)))
+           (.log js/console interval (<! (timeout interval)))
+           ))))
+
+(defn send-sounds []
+  (go (dotimes [_ 800]
+        (>! sound-chan "note"))))
+
+(play-timer)
+(send-sounds)
 
 
