7f5c106036095388c31adb8705c8ddf35e051ce6
Julia Evans <julia@jvns.ca>
2013-11-25 23:30:09 -0500

    Add ClojureScript dependencies

--- project.clj
+++ project.clj
@@ -4,4 +4,15 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [overtone "0.9.0-SNAPSHOT"]])
+                 [overtone "0.9.0-SNAPSHOT"]
+                 [org.clojure/clojurescript "0.0-1934"
+                  :exclusions [org.apache.ant/ant]]]
+  :source-paths ["src"]
+  :plugins [[lein-cljsbuild "0.3.4"]]
+  :cljsbuild
+  {:builds
+   [{:id "dev"
+     :source-paths ["src"]
+     :compiler {:output-to "out/browser/main.js"
+                :output-dir "out/browser"
+                :optimizations :none}}]})
