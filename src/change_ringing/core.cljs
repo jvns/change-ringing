a1f6bd618f13693d7c7ecf8312b3accd610df687
Julia Evans <julia@jvns.ca>
2013-11-26 00:04:03 -0500

    I MADE A SOUND

--- src/change_ringing/core.cljs
+++ src/change_ringing/core.cljs
@@ -1,2 +1,13 @@
 (ns change-ringing.core)
 (js/console.log (+ 3 2))
+
+(def api-key "b1de6c92bba14d96a9fd957696c8cc4e")
+
+(set! (.-apiKey js/freesound) api-key)
+(. js/freesound
+   (get_sound 148432
+              (fn [sound] (-> sound
+                          (aget "preview-hq-mp3")
+                          (js/Audio.)
+                          (.play)))))
+
