def960bc80e08ea7d91713538f66f9243a54d1ab
Julia Evans <julia@jvns.ca>
2013-11-26 00:22:20 -0500

    Put freesound code into function

--- src/change_ringing/core.cljs
+++ src/change_ringing/core.cljs
@@ -3,11 +3,18 @@
 
 (def api-key "b1de6c92bba14d96a9fd957696c8cc4e")
 
+(def sound-cache (js/Object.))
+
 (set! (.-apiKey js/freesound) api-key)
-(. js/freesound
-   (get_sound 148432
-              (fn [sound] (-> sound
-                          (aget "preview-hq-mp3")
-                          (js/Audio.)
-                          (.play)))))
+(defn get-sound [name id]
+  (. js/freesound
+     (get_sound id
+                (fn [sound]
+                  (let [snd (-> sound
+                                (aget "preview-hq-mp3")
+                                (js/Audio.))]
+                    (aset sound-cache name sound))))))
+
+;; (get-sound "C4" 143214)
+
 
