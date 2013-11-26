4f186d40f5cbee8820adc06757331a6047f90917
Julia Evans <julia@jvns.ca>
2013-11-26 11:48:54 -0500

    Use new awesome cljsbuild version

--- project.clj
+++ project.clj
@@ -5,10 +5,11 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [overtone "0.9.0-SNAPSHOT"]
-                 [org.clojure/clojurescript "0.0-1934"
-                  :exclusions [org.apache.ant/ant]]]
+                 [org.clojure/clojurescript "0.0-2014"
+                  :exclusions [org.apache.ant/ant]]
+                 [org.clojure/core.async "0.1.242.0-44b1e3-alpha"]]
   :source-paths ["src"]
-  :plugins [[lein-cljsbuild "0.3.4"]]
+  :plugins [[lein-cljsbuild "1.0.0-alpha1"]]
   :cljsbuild
   {:builds
    [{:id "dev"
